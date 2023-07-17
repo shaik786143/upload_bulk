ec0064da92e5505f50dbae129f543d1e74b6b346
Aashish <aashishkumar12376@gmail.com>
2023-07-17 16:23:40 +0530

    added project.

--- app/views.py
+++ app/views.py
@@ -5,7 +5,7 @@ from django.contrib import messages
 def index(request):
     if request.method == "POST":
         name = request.POST.get("name")
-        documents = request.POST.getlist("documents")
+        documents = request.FILES.getlist("documents")
         documents_list = [ Document(name=name, document=document) for document in documents]
         Document.objects.bulk_create(documents_list)
 
